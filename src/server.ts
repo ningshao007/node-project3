f527c1590e8026547cc4f1882c108d1579e2593c
令狐少侠 <xxx@yy.com>
2022-12-15 14:34:47 +0800

    feat: 连接mongodb数据库

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,9 @@
+import 'dotenv/config';
 import App from './app';
 import PostsController from './posts/posts.controller';
+import validateEnv from './utils/validateEnv';
+
+// validateEnv();
 
 const app = new App([new PostsController()], 3000);
 
