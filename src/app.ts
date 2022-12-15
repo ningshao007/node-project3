2c87cad3642a99f2ceba18da407cc6413fb96447
令狐少侠 <xxx@yy.com>
2022-12-15 15:06:41 +0800

    refactor: 添加bodyParser参数

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ class App {
 
 	private initializeMiddlewares() {
 		this.app.use(bodyParser.json());
-		this.app.use(bodyParser.urlencoded());
+		this.app.use(bodyParser.urlencoded({ extended: false }));
 	}
 
 	private initializeControllers(controllers) {
