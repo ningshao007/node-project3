a4aa957383175965718e22273ed59c0e62c7ebf6
令狐少侠 <xxx@yy.com>
2022-12-16 08:26:59 +0800

    refactor: post model

--- src/posts/post.dto.ts
+++ src/posts/post.dto.ts
@@ -2,8 +2,8 @@ import { IsMongoId, IsString } from 'class-validator';
 
 class CreatePostDto {
 	// NOTE: 关联mongoId
-	@IsMongoId()
-	public author: string;
+	// @IsMongoId()
+	// public author: string;
 
 	@IsString()
 	public content: string;
